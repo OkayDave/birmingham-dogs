aaa197974293a5b2172bcbddf09e106fee546112
Dave Russell <dave.kerr@gmail.com>
2015-08-01 07:57:35 +0100

    navigation + placeholders

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+
+  resources :dogs, only: [:index, :show]
+  resources :shelters, only: [:index, :show]
+  resources :breeds, only: [:index, :show]
+
   root to: 'visitors#index'
 end
